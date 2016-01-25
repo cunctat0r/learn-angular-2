5847150c194871dc83c7e0f1e12332844e583952
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-25 15:16:04 +0300

    Create controller

---
+++ public/vendor/js/app/controllers/customersController.js
@@ -0,0 +1,15 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('CustomersController', ['$scope', function($scope) {
+  $scope.sortBy = 'name';
+  $scope.reverse = false;
+  $scope.customers = [{name:'John', city:'London', joined:'2014-12-02', orderTotal:212.5}, 
+			{name:'Paul', city:'Liverpool', joined:'2014-11-02', orderTotal:312.1}, 
+			{name:'George', city:'Glasgow', joined:'2014-10-02', orderTotal:435.99}, 
+			{name:'Leo', city:'Albuquerque', joined:'2014-01-02', orderTotal:415.99}, 
+			{name:'Ringo', city:'Manchester', joined:'2014-11-12', orderTotal:111}];
+  $scope.doSort = function (propName) {
+	$scope.sortBy = propName;
+	$scope.reverse = !$scope.reverse
+  };			
+}]);
