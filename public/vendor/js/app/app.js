2cd45eb3f1d7a2b170f5b071161d914f1f61c423
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-01 10:14:12 +0300

    Add angularJS view 'customers'

--- public/vendor/js/app/app.js
+++ public/vendor/js/app/app.js
@@ -1,4 +1,16 @@
 (function() {
 	var myApp = angular.module('myApp', ['ngRoute']);	
+
+	myApp.config(function($routeProvider) {
+		$routeProvider
+			.when('/',
+			{
+				controller: 'CustomersController',
+				templateUrl: '/vendor/js/app/views/customers.html'
+			})			
+			.otherwise({redirectTo: '/'});
+
+	});
+
 }());
 
